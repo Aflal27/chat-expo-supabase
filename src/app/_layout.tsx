47e2699bb2d837546b31a7da3f4f4692cb584880
Aflal <mohamedaflal6719@gmail.com>
2025-02-01 21:05:42 +0530

    prebuild

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,20 @@
 import { Slot, Stack } from 'expo-router'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import AuthProvider from '../providers/authProvider'
+import { useEffect } from 'react'
+import { PermissionsAndroid, Platform } from 'react-native'
 
 export default function RootLayout() {
+  useEffect(() => {
+    const run = async () => {
+      if (Platform.OS === 'android') {
+        await PermissionsAndroid.requestMultiple([
+          'android.permission.POST_NOTIFICATIONS',
+          'android.permission.BLUETOOTH_CONNECT',
+        ])
+      }
+    }
+  })
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <AuthProvider>
